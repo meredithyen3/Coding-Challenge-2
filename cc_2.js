01dda59fc5e0a29fd97df54e52f0186d38b3966a
meredithyen3 <meredithyen@usf.edu>
2025-01-26 13:00:33 -0500

    Task - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+// Task 1: Store Inventory
+let products = ["Notebook", "Pencils", "Computers", "Backpacks", "Books"]
+console.log(products);
+products.push("Highlighters");
+console.log(products);
+console.log(products.pop());
