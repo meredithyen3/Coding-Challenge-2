b8dc5e9c4bc6065aff4d974b0b056291d327e353
meredithyen3 <meredithyen@usf.edu>
2025-01-26 13:34:57 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,19 @@ console.log(products);
 products.push("Highlighters");
 console.log(products);
 console.log(products.pop());
+
+
+// Task 2: Student Scores
+let scores = [90, 92, 78, 65, 100];
+console.log(scores);
+scores[1] = 94;
+console.log(scores);
+function calculateAverage(scores) {
+    let sum = 0;
+    for (let i = 0; i < scores.length; i++) {
+        sum += scores[i];
+    }
+    return sum / scores.length;
+}
+const average = calculateAverage(scores);
+console.log(average);
